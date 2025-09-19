90deefbb535e573d925c003085eb27fad64cc58f
Sebobs <sebbe-bjorn@live.dk>
2025-09-19 17:23:39 +0200

    Made a working Options menu

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public ScenePlayer ScenePlayer;
+    public GameObject OptionsPanel;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        ScenePlayer = GetComponent<ScenePlayer>();
+    }
+
+    public void OptionsPressed()
+    {
+        OptionsPanel.SetActive(true);
+    }
+
+    public void OptionsClose()
+    {
+        OptionsPanel.SetActive(false);
+    }
+
+}
