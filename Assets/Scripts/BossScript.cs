4babbc1d6e2166bf06167b50ad90467b67a0e3e9
MadsKrogsgaard <mckrogsgaard@gmail.com>
2025-09-26 14:58:42 +0200

    Update BossScript.cs

--- Assets/Scripts/BossScript.cs
+++ Assets/Scripts/BossScript.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class BossScript : MonoBehaviour
 {
+    public int bossMaxHealth;
+    public int bossCurrentHealth;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
