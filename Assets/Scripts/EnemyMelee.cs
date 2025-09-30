937f6871c710be94aeec0d2f2c37e8311ff604eb
Sebobs <sebbe-bjorn@live.dk>
2025-09-30 15:58:38 +0200

    MeleeEnemy is ready to be integrated

--- Assets/Scripts/EnemyMelee.cs
+++ Assets/Scripts/EnemyMelee.cs
@@ -5,11 +5,13 @@ using System;
 public class MeleEnemy : MonoBehaviour
 {
     public EnemyAI enemyAI;
+    public EnemyHealthManager hpManager;
     public float cooldown = 3.0f;
     Timer cooldownTimer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        hpManager = GetComponent<EnemyHealthManager>();
         enemyAI = GetComponent<EnemyAI>();
         enemyAI.distance = 0;
         cooldownTimer = new Timer(cooldown*1000);
@@ -25,6 +27,7 @@ public class MeleEnemy : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             Debug.Log("Melee mode activated");      //enemy is attacking player
+            /*collision.gameObject.GetComponent<PlayerHealthManager>().TakeDamage((int)1);*/ //does 1 damage to player
             cooldownTimer.Enabled = true;           //cooldown timer started
             enemyAI.still = true;
         }
