d88191d26afefee66ccaa557ec9821969d521905
Sebobs <sebbe-bjorn@live.dk>
2025-09-30 15:06:57 +0200

    EnemyAI can now run away

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,32 +1,33 @@
 using System;
 using Unity.Hierarchy;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using static UnityEngine.GraphicsBuffer;
 
 public class EnemyAI : MonoBehaviour
 {
-    public Rigidbody myrigidbody;   //makes a public rigidbody variable
-    public float startSpeed;        //makes a public startSpeed variable
-    public float movementSpeed;     //makes a public movementSpeed variable
-    Transform finalTarget;          //makes a transform variable which gives us our target
-    Vector3 angle;                  //makes a vector3 variable to describe an angle
-    GameObject p1;                  //gets player1 as a variable
-    GameObject p2;                  //gest player2 as a variable
-    public double distance = 0;
-    public bool still = false;
+    public Rigidbody myrigidbody;                   //makes a public rigidbody variable
+    public float startSpeed, movementSpeed;         //makes a public startSpeed variable //makes a public movementSpeed variable
+    Transform finalTarget;                          //makes a transform variable which gives us our target
+    Vector3 angle;                                  //makes a vector3 variable to describe an angle
+    GameObject p1, p2;                  //gets player1 as a variable      //gets player2 as a variable
+    public double distance = 0, distanceOffset = 0.2; //sets distance and distanceOffset
+    public bool still = false;                      //makes a public bool, if true enemy stands still
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         movementSpeed = startSpeed;                 //sets our current movement speed as our start movement speed
         myrigidbody = GetComponent<Rigidbody>();    //sets our rigidbody
         myrigidbody.useGravity = false;             //turns off gravity for our rigidbody
+        myrigidbody.freezeRotation = true;          //freezes rotation of enemy
         p1 = GameObject.Find("Player1");            //sets player1 as p1
         p2 = GameObject.Find("Player2");            //setes player2 as p2
-        myrigidbody.freezeRotation = true;
     }
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space)) MoveEnemy();
         if (!still)
         {
             if (Vector2.Distance(p1.transform.position, myrigidbody.transform.position) <
@@ -42,13 +43,17 @@ public class EnemyAI : MonoBehaviour
             angle = finalTarget.position - myrigidbody.transform.position;  //finds the difference between our position, and the target position
             angle.Normalize();      //normalizes the angle (makes it into a 1vector)
             
-            if (Vector2.Distance(finalTarget.position, myrigidbody.transform.position) > distance)
+            if (distance+distanceOffset > Vector2.Distance(finalTarget.position, myrigidbody.transform.position) && Vector2.Distance(finalTarget.position, myrigidbody.transform.position) > distance - distanceOffset)
             {
-                myrigidbody.transform.position += (angle * movementSpeed*Time.deltaTime); //moves enemy towards the target
+                still = true;           //stops enemy if they are within the offset distance
+            }
+            else if (distance > Vector2.Distance(finalTarget.position, myrigidbody.transform.position))
+            {
+                myrigidbody.transform.position -= (angle * movementSpeed * Time.deltaTime);         //moves enemy away from target
             }
             else
             {
-                still = true;
+                myrigidbody.transform.position += (angle * movementSpeed * Time.deltaTime);        //moves enemy towards target
             }
         }
     }
@@ -57,5 +62,8 @@ public class EnemyAI : MonoBehaviour
         //If enemy comes into contact with a player, they know
         if (collision.gameObject.CompareTag("Player")) Debug.Log("They hit the second tower");
     }
-
+    public void MoveEnemy()      //If Called, enemy moves again. Should attack before this is called
+    {
+        still = false;
+    }
 }
