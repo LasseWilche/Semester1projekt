30fd75818f0794fa77794ea075be1e01db715186
MadsKrogsgaard <mckrogsgaard@gmail.com>
2025-11-04 11:32:39 +0100

    Adding boss patrol ai

--- Assets/Scripts/BossScript.cs
+++ Assets/Scripts/BossScript.cs
@@ -1,24 +1,64 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Rendering;
 
-public class BossScript : MonoBehaviour
+public class BossScript : HealthManager
 {
-    //BOSS HEALTH VARIABLES
-    public int bossMaxHealth;
-    public int bossCurrentHealth;
+    //Boss Patrol variables
+    public float moveSpeed;
+    public Transform[] patrolPoints;
+    public float waitTime;
+    int currentPointIndex;
+    bool once;
 
     //BOSS ATTACK VARIABLES
     //Makes sure bullet gets destroyed after time
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public override void Start()
     {
-        bossCurrentHealth = bossMaxHealth;
+        base.Start();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
+    {
+        if (transform.position != patrolPoints[currentPointIndex].position)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[currentPointIndex].position, moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            if (once == false)
+            {
+                once = true;
+                StartCoroutine(Wait());
+            }
+        }
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(waitTime);
+        if (currentPointIndex + 1 < patrolPoints.Length)
+        {
+            currentPointIndex++;
+        }
+        else
+        {
+            currentPointIndex = 0;
+        }
+        once = false;
+    }
+
+    public override void DieAnimation()
     {
 
+    }
+
+    public override void Dying()
+    {
+        
     }
 }
