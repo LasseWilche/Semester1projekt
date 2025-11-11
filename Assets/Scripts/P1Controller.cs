9ea529fe2c0ce3ea1eeaa755bc02536b8528bd05
MadsKrogsgaard <mckrogsgaard@gmail.com>
2025-11-11 14:44:54 +0100

    Fix af collider med player & walls

--- Assets/Scripts/P1Controller.cs
+++ Assets/Scripts/P1Controller.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering.Universal;
 
 public class P1Controller : MonoBehaviour
 
 {
+    public Rigidbody2D rb2d;
     public float moveSpeed = 10f;
 
     void Start()
@@ -13,7 +15,8 @@ public class P1Controller : MonoBehaviour
 
     void Update()
     {
-        Vector3 move = Vector3.zero;
+        p1Movement();
+     /*   Vector3 move = Vector3.zero;
 
         if (Keyboard.current.iKey.isPressed)
             move += Vector3.up;
@@ -27,7 +30,31 @@ public class P1Controller : MonoBehaviour
         if (move != Vector3.zero)
         {
             move.Normalize();
-            transform.position += move * moveSpeed * Time.deltaTime;
+            rb2d.linearVelocity = move * moveSpeed * Time.deltaTime;
+        }*/
+    }
+
+    private void p1Movement()
+    {
+        Vector3 move = Vector3.zero;
+
+        if (Keyboard.current.iKey.isPressed)
+        {
+            rb2d.AddForce(Vector2.up * moveSpeed);
         }
+        if (Keyboard.current.kKey.isPressed)
+        {
+            rb2d.AddForce(Vector2.down * moveSpeed);
+        }
+        if (Keyboard.current.jKey.isPressed)
+        {
+            rb2d.AddForce(Vector2.left * moveSpeed);
+        }
+        if (Keyboard.current.lKey.isPressed)
+        {
+            rb2d.AddForce(Vector2.right * moveSpeed);
+        }
+        move.Normalize();
+        rb2d.linearVelocity = move * moveSpeed;
     }
 }
