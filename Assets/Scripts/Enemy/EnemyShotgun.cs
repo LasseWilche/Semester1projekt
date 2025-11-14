3b989787bcd06f9a12c01bc37cf0d3a511fd908b
haraldtk315 <haraldtk315@gmail.com>
2025-11-14 11:09:08 +0100

    fix

--- Assets/Scripts/Enemy/EnemyShotgun.cs
+++ Assets/Scripts/Enemy/EnemyShotgun.cs
@@ -3,13 +3,10 @@ using UnityEngine.Audio;
 
 public class EnemyShotgun : EnemyBaseRanged
 {
-    public AudioClip shotsound1;
-    public AudioClip shotsound2;
+ 
     public EnemyShotgun() : base(5, 5, 15)
     {
-        int i = Random.Range(0, 3);
-        AudioClip randomClip = (i == 0) ? MeleeSound1 : (i == 1) ? MeleeSound2 : MeleeSound3;
-        audioSource.PlayOneShot(randomClip);
+        
 
         cooldown = 5;
         movementSpeed = 3;
