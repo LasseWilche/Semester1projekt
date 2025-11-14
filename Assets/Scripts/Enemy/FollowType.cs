3eaec59b3118d5a5befbb4069009c18ad4045f0c
Sebobs <sebbe-bjorn@live.dk>
2025-11-14 13:33:07 +0100

    Enemies now bounce

--- Assets/Scripts/Enemy/FollowType.cs
+++ Assets/Scripts/Enemy/FollowType.cs
@@ -7,24 +7,28 @@ public class FollowType : EnemyBaseClass
     {
     }
 
-    public override IEnumerator AttackScript(Collision collision)
+    public override IEnumerator AttackScript()
     {
         animator.Play("Melee");
         yield return new WaitForSeconds(0.5f);
-        collision.gameObject.GetComponent<PlayerHealthManager>().TakeDamage(1);
+        if (target.gameObject.GetComponent<PlayerHealthManager>() != null)
+        {
+            target.gameObject.GetComponent<PlayerHealthManager>().TakeDamage(1);
+        }
+        else Debug.Log("Player has no health manager");
     }
     public override void MovementScript()
     {
         base.MovementScript();
         angle = target.position - myrb.transform.position;  //finds the difference between our position, and the target position
         angle.Normalize();      //normalizes the angle (makes it into a 1vector)
-        myrb.transform.position += (movementSpeed * Time.deltaTime * angle);
+        myrb.linearVelocity = movementSpeed * angle;
     }
-    public void OnCollisionEnter(Collision collision)
+    public void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player")||alive)
+        if (collision.gameObject.CompareTag("Player") && alive)
         {
-            AttackScript(collision);
+            StartCoroutine(AttackScript());
             cooldown = 3;
         }
     }
