190c14b65dff7b11592ea5cdd7bb21441b0062ef
Sebobs <sebbe-bjorn@live.dk>
2025-12-02 13:03:26 +0100

    SmallFixes

--- Assets/Scripts/Enemy/EnemyBulletScript.cs
+++ Assets/Scripts/Enemy/EnemyBulletScript.cs
@@ -1,15 +1,14 @@
 using UnityEngine;
 public class EnemyBulletScript : BasicBulletScript
 {
-    private void OnTriggerEnter(Collider collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-
-        Debug.Log("hit");
-        if (collision.gameObject.CompareTag("Wall") || collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player"))
         {
-            
+            PlayerHealthManager playerHealthManager = collision.GetComponent<PlayerHealthManager>();
+            playerHealthManager.TakeDamage(1);
             Destroy(this.gameObject);
-
         }
+        if (collision.gameObject.CompareTag("Wall")) Destroy(this.gameObject);
     }
 }
