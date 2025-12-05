7546069fce009ce7e06b248969fa1ddd07ff9c67
Sebobs <sebbe-bjorn@live.dk>
2025-12-05 10:43:33 +0100

    Fixes slime bullets again

--- Assets/Scripts/Enemy/EnemyBulletScript.cs
+++ Assets/Scripts/Enemy/EnemyBulletScript.cs
@@ -1,15 +1,16 @@
 using UnityEngine;
 public class EnemyBulletScript : BasicBulletScript
 {
-    private void OnTriggerEnter(Collider collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
 
         Debug.Log("hit");
-        if (collision.gameObject.CompareTag("Wall") || collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player"))
         {
-            
+            collision.GetComponent<PlayerHealthManager>().TakeDamage(1);
             Destroy(this.gameObject);
 
         }
+        else if (collision.gameObject.CompareTag("Wall")) Destroy(this.gameObject);
     }
 }
